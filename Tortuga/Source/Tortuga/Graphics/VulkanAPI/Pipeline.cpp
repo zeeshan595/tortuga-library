c1f7b1ed3cf2630aef47152e013d56eaef559e10
Zeeshan Abid <zeeshan595@googlemail.com>
2019-02-11 01:30:31 +0000

    Inverted Depth Buffer

--- Tortuga/Source/Tortuga/Graphics/VulkanAPI/Pipeline.cpp
+++ Tortuga/Source/Tortuga/Graphics/VulkanAPI/Pipeline.cpp
@@ -68,8 +68,8 @@ Pipeline::Pipeline(Device *device, Swapchain *swapchain, RenderPass *renderPass,
         rasterizer.rasterizerDiscardEnable = VK_FALSE;
         rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
         rasterizer.lineWidth = 1.0f;
-        rasterizer.cullMode = VK_CULL_MODE_NONE;
-        rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+        rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
         rasterizer.depthBiasEnable = VK_FALSE;
         rasterizer.depthBiasConstantFactor = 0.0f; // Optional
         rasterizer.depthBiasClamp = 0.0f;          // Optional
@@ -127,13 +127,13 @@ Pipeline::Pipeline(Device *device, Swapchain *swapchain, RenderPass *renderPass,
         depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
         depthStencil.depthTestEnable = VK_TRUE;
         depthStencil.depthWriteEnable = VK_TRUE;
-        depthStencil.depthCompareOp = VK_COMPARE_OP_LESS;
+        depthStencil.depthCompareOp = VK_COMPARE_OP_GREATER;
         depthStencil.depthBoundsTestEnable = VK_FALSE;
-        depthStencil.minDepthBounds = 0.0f; // Optional
-        depthStencil.maxDepthBounds = 1.0f; // Optional
-        depthStencil.stencilTestEnable = VK_FALSE;
-        depthStencil.front = {}; // Optional
-        depthStencil.back = {};  // Optional
+        depthStencil.minDepthBounds = 0.0f;
+        depthStencil.maxDepthBounds = 1.0f;
+        depthStencil.stencilTestEnable = VK_TRUE;
+        depthStencil.front = {};
+        depthStencil.back = {};
     }
 
     //Dynamic states
