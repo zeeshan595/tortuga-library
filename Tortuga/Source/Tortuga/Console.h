d8f83a103bb2ef14e3009788b08cd361d297d3fe
Zeeshan Abid <zeeshan595@googlemail.com>
2019-02-15 19:54:30 +0000

    Console Logger Updated

--- Tortuga/Source/Tortuga/Console.h
+++ Tortuga/Source/Tortuga/Console.h
@@ -1,5 +1,5 @@
-#ifndef _CONSOLE
-#define _CONSOLE
+#ifndef _TORTUGA_CONSOLE
+#define _TORTUGA_CONSOLE
 
 #include "Core.h"
 
@@ -7,50 +7,6 @@ namespace Tortuga
 {
 namespace Console
 {
-class Logger
-{
-private:
-  std::ostringstream _message;
-
-public:
-  Logger(bool b)
-  {
-    if (b)
-      _message << "True";
-    else
-      _message << "False";
-  }
-  Logger(int i)
-  {
-    _message << i;
-  }
-  Logger(uint32_t i)
-  {
-    _message << i;
-  }
-  Logger(const char *str)
-  {
-    _message << str;
-  }
-  Logger(char str[])
-  {
-    _message << str;
-  }
-  Logger(std::string str)
-  {
-    _message << str;
-  }
-
-  std::string str()
-  {
-    return _message.str();
-  }
-  const char *c_str()
-  {
-    return _message.str().c_str();
-  }
-};
-
 const int TEXT_BLACK = 30;
 const int TEXT_RED = 31;
 const int TEXT_GREEN = 32;
@@ -68,47 +24,110 @@ const int BG_BLUE = 44;
 const int BG_PURPLE = 45;
 const int BG_CYAN = 46;
 const int BG_WHITE = 47;
-
-std::string MakeString(std::string message, std::vector<Logger> args)
+class Arguments
 {
-  if (args.size() == 0)
-    return message;
+private:
+  std::vector<std::string> _args;
 
-  for (uint32_t i = 0; i < args.size(); i++)
+public:
+  Arguments()
   {
-    std::ostringstream argString;
-    argString << "{" << i << "}";
-    auto index = message.find(argString.str());
-    message.replace(index, index + argString.str().size(), args[i].str());
+  }
+  Arguments(bool b)
+  {
+    *this << b;
+  }
+  Arguments(int i)
+  {
+    *this << i;
+  }
+  Arguments(uint32_t i)
+  {
+    *this << i;
+  }
+  Arguments(const char str)
+  {
+    *this << str;
+  }
+  Arguments(const char *str)
+  {
+    *this << str;
+  }
+  Arguments(std::string str)
+  {
+    *this << str;
   }
 
-  return message;
-}
+  std::vector<std::string> str()
+  {
+    return _args;
+  }
 
-void Info(std::string message, std::vector<Logger> args = {})
-{
-#if DEBUG_MODE
-  std::cout << "\033[" << BG_BLACK << ";" << TEXT_WHITE << "m" << MakeString(message, args) << " \033[0m" << std::endl;
-#endif
-}
+  Arguments &operator<<(bool b)
+  {
+    std::ostringstream stream;
+    if (b)
+      stream << "True";
+    else
+      stream << "False";
 
-void Warning(std::string message, std::vector<Logger> rest = {})
+    _args.push_back(stream.str());
+    return *this;
+  }
+  Arguments &operator<<(int i)
+  {
+    std::ostringstream stream;
+    stream << i;
+    _args.push_back(stream.str());
+    return *this;
+  }
+  Arguments &operator<<(uint32_t i)
+  {
+    std::ostringstream stream;
+    stream << i;
+    _args.push_back(stream.str());
+    return *this;
+  }
+  Arguments &operator<<(const char c)
+  {
+    std::ostringstream stream;
+    stream << c;
+    _args.push_back(stream.str());
+    return *this;
+  }
+  Arguments &operator<<(const char *str)
+  {
+    std::ostringstream stream;
+    stream << str;
+    _args.push_back(stream.str());
+    return *this;
+  }
+  Arguments &operator<<(std::string str)
+  {
+    std::ostringstream stream;
+    stream << str;
+    _args.push_back(stream.str());
+    return *this;
+  }
+};
+enum Level
 {
-#if DEBUG_MODE
-  std::cout << "\033[" << BG_BLACK << ";" << TEXT_YELLOW << "m" << MakeString(message, rest) << " \033[0m" << std::endl;
-#endif
-}
+  LEVEL_NONE,
+  LEVEL_INFO,
+  LEVEL_DEBUG,
+  LEVEL_WARNING,
+  LEVEL_ERROR,
+  LEVEL_FATAL
+};
 
-void Error(std::string message, std::vector<Logger> rest = {})
-{
-  std::cout << "\033[" << BG_BLACK << ";" << TEXT_RED << "m" << MakeString(message, rest) << " \033[0m" << std::endl;
-}
+void ProcessLog(Level level, std::string message, Arguments args);
 
-void Fatal(std::string message, std::vector<Logger> rest = {})
-{
-  std::cout << "\033[" << BG_BLACK << ";" << TEXT_RED << "m" << MakeString(message, rest) << " \033[0m" << std::endl;
-  throw std::runtime_error(message);
-}
+void Print(std::string message, Arguments args = {});
+void Info(std::string message, Arguments args = {});
+void Debug(std::string message, Arguments args = {});
+void Warning(std::string message, Arguments args = {});
+void Error(std::string message, Arguments args = {});
+void Fatal(std::string message, Arguments args = {});
 }; // namespace Console
 }; // namespace Tortuga
 
