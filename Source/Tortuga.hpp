f280e21f531f13d7d6dbbe88e8b228fdd7d9c2d2
Zeeshan <mail@zeeshan-abid.com>
2019-07-17 01:16:49 +0100

    Header files updated

--- Source/Tortuga.hpp
+++ Source/Tortuga.hpp
@@ -1,12 +1,22 @@
 #ifndef _TORTUGA
 #define _TORTUGA
 
-#include "Tortuga/Console.hpp"
+#include "./Tortuga/Console.hpp"
 
 //Utils
-#include "Tortuga/Utils/IO.hpp"
+#include "./Tortuga/Utils/IO.hpp"
 
 //Internal Graphics API
-#include "Tortuga/Graphics/Vulkan.hpp"
+#include "./Tortuga/Graphics/Vulkan/ErrorCheck.hpp"
+#include "./Tortuga/Graphics/Vulkan/Instance.hpp"
+#include "./Tortuga/Graphics/Vulkan/Device.hpp"
+#include "./Tortuga/Graphics/Vulkan/Window.hpp"
+#include "./Tortuga/Graphics/Vulkan/Swapchain.hpp"
+#include "./Tortuga/Graphics/Vulkan/Shader.hpp"
+#include "./Tortuga/Graphics/Vulkan/Image.hpp"
+#include "./Tortuga/Graphics/Vulkan/Fence.hpp"
+#include "./Tortuga/Graphics/Vulkan/Buffer.hpp"
+#include "./Tortuga/Graphics/Vulkan/CommandPool.hpp"
+#include "./Tortuga/Graphics/Vulkan/Command.hpp"
 
 #endif
