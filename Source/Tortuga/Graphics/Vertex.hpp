614825d41bb69cdf1f1083fe04bdfc80b839b293
Zeeshan Abid <mail@zeeshan-abid.com>
2019-08-28 02:13:13 +0100

    Bug Fix

--- Source/Tortuga/Graphics/Vertex.hpp
+++ Source/Tortuga/Graphics/Vertex.hpp
@@ -34,7 +34,7 @@ struct Vertex
     {
       attributeDescriptions[0].binding = 0;
       attributeDescriptions[0].location = 0;
-      attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+      attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
       attributeDescriptions[0].offset = offsetof(Vertex, Position);
 
       attributeDescriptions[1].binding = 0;
