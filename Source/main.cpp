a5911c902e63d5f7656d0f171d95f44630bb80ef
Zeeshan Abid <mail@zeeshan-abid.com>
2019-09-29 19:42:15 +0100

    input events

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,15 +5,29 @@ bool ShouldClose = false;
 
 int main()
 {
+  //setup engine
   Core::Engine::Create();
+
+  //setup events (can use these anywhere)
   Core::Input::NotifyOnWindowClose([] {
     ShouldClose = true;
   });
+  Core::Input::NotifyOnKeyEvent([](const auto key, const auto event) {
+    if (key == Core::Input::KeyCode::Space)
+      ShouldClose = true;
+  });
+
+  //create a cube with a mesh component
+  const auto cube = Core::Engine::CreateEntity();
+  const auto obj = Utils::IO::LoadObjFile("Assets/Models/Cube.obj");
+
   Core::Engine::AddSystem<Systems::Rendering>();
+
+  //main loop
   while (!ShouldClose)
-  {
     Core::Engine::IterateSystems();
-  }
+
+  //auto destroys everything
   Core::Engine::Destroy();
   return EXIT_SUCCESS;
 }
